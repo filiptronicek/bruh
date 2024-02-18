064914b13a793e9e9bf65a0be09754af94457f6f
Filip Troníček <filip@gitpod.io>
2024-02-18 22:25:39 +0100

    Away with the old stuff

--- bin/src/availible.js
+++
@@ -1,7 +0,0 @@
-const dns = require("dns");
-
-module.exports = (url) => {
-  dns.resolve4(url, (_err, addresses) => {
-    return addresses === undefined;
-  });
-};
--- test/availible.test.js
+++
@@ -1,5 +0,0 @@
-const isAvailable = require('../bin/src/availible');
-
-test('tries to resolve google.com', () => {
-    expect(isAvailable('google.com')).toBe(undefined);
-});
